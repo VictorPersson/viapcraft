c5bda45aeb1d0e4231f943b85cb4794ab414425b
VictorPersson <vp-96@hotmail.com>
2021-02-20 19:17:27 +0100

    Initial push

---
+++ mod-files/src/main/java/com/vp/viapcraft/Viapcraft.java
@@ -0,0 +1,13 @@
+package com.vp.viapcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Viapcraft implements ModInitializer {
+
+    public static final String MOD_ID = "vpc";
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
